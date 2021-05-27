dc843e2af7fbb97ddbea4e39c3291f07095079e7
Dmitry Zuiko <d.zuiko.vironit@vironit.ru>
2021-05-27 17:44:32 +0300

    validation on name

--- services/adminsService.js
+++ services/adminsService.js
@@ -5,21 +5,26 @@ const jwt = require('jsonwebtoken');
 class AdminsService {
     registerNewAdmin = async(admin) => {
         try {
-            console.log(admin);
-            const hashPass = await bcrypt.hash(admin.password, 10);
-            const newUser = new Admin({
-                name: admin.name,
-                phone: admin.phone,
-                password: hashPass,
-            });
-            await newUser.save();
+            const isAdmin = await Admin.findOne({name: admin.name});
+            if (!isAdmin) {
+                const hashPass = await bcrypt.hash(admin.password, 10);
+                const newUser = new Admin({
+                    name: admin.name,
+                    phone: admin.phone,
+                    password: hashPass,
+                });
+                await newUser.save();
+            } else {
+                return {
+                    message: "Admin with this name has already been created"
+                };
+            }
         } catch (e) {
             console.log(e);
         }
     };
     login = async(name, password) => {
         try {
-
             const admin = await Admin.findOne({name});
             if (admin) {
                 const check = await bcrypt.compare(password, admin.password);
