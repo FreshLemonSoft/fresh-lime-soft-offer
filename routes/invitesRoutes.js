83d9cc301e496442085e754cdafb430f360d9b77
Dmitry Zuiko <d.zuiko.vironit@vironit.ru>
2021-06-03 11:51:43 +0300

    remove auth in get request

--- routes/invitesRoutes.js
+++ routes/invitesRoutes.js
@@ -1,9 +1,8 @@
 const {Router} = require('express');
 const controller = require('../controllers/invitesController');
-const auth = require('../middleware/authMiddleware');
 
 const router = Router();
 
-router.get('/:id', auth, controller.getUserById);
+router.get('/:id', controller.getUserById);
 
 module.exports = router;
